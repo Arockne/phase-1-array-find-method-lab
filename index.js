ce2a13e0a933b4d7ab4e4a1549c3abe9164ecc3d
Daniel Calvin <arockne2003@yahoo.com>
2021-07-22 19:39:08 -0700

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+//implement superbowlWin
+  //passes an array of objects
+    //has year and result
+  //use find method to find the object with a result of 'W'
+  //return the year the win occurred
+function superbowlWin(record) {
+  const win = record.find(superbowl => superbowl.result === 'W');
+  return win && win.year;
+}
